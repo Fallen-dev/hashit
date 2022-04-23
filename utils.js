0727f1fbe20a7d3dc9ffaef687471d2fa8d607ad
asweetrain <rishav.m.19.02@gmail.com>
2022-04-23 23:17:33 +0530

    remove console.log

--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
 export const getElement = (selector, type = document) => type.querySelector(selector);
-export const getAllElement = (selector, type=document) => [...type.querySelectorAll(selector)]
+export const getAllElement = (selector, type = document) => [...type.querySelectorAll(selector)]
 export const log = (...args) => console.debug(...args); //debug
-export const log_err = (...args) => console.error(...args); //debug
 
 export const toString = (value) => JSON.stringify(value);
 export const toObject = (value) => JSON.parse(value);
