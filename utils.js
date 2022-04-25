d91975810ebbb195c7dfa76860b1b3240356ba25
asweetrain <rishav.m.19.02@gmail.com>
2022-04-25 23:08:11 +0530

    add new isEmpty()

--- utils.js
+++ utils.js
@@ -7,3 +7,5 @@ export const toObject = (value) => JSON.parse(value);
 
 export const save_to_storage = (key, list) => localStorage.setItem(key, list);
 export const get_from_storage = (key) => localStorage.getItem(key);
+
+export const isEmpty = (str) => !str || /^\s*$/.test(str)
